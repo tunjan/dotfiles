463ae91f25bc01dd9cc338620e9d56760d31cd60
tunjan <tunjan@autistici.org>
2023-09-18 16:32:13 +0200

    second commit

---
+++ dot_local/src/dwmblocks/config.h
@@ -0,0 +1,36 @@
+
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"⌨", "sb-kbselect", 0, 30}, */
+	// {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	// {"",	"sb-tasks",	10,	26},
+	/* {"",	"sb-music",	0,	11}, */
+	// {"",	"sb-pacpackages",	0,	8},
+	// {"",	"sb-news",		0,	6},
+	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
+	/* {"",	"sb-price eth Ethereum 🍸 23",	9000,	23}, */
+	/* {"",	"sb-price btc Bitcoin 💰 21",				9000,	21}, */
+	// {"",	"sb-torrent",	20,	7},
+	/* {"",	"sb-memory",	10,	14}, */
+	 // {"",	"sb-cpu",		10,	18},
+	/* {"",	"sb-moonphase",	18000,	17}, */
+	// {"",	"sb-doppler",	0,	13},
+	// {"",	"sb-forecast",	18000,	5},
+	// {"",	"sb-mailbox",	180,	12},
+	// {"",	"sb-nettraf",	1,	16},
+	// {"",	"sb-volume",	0,	10},
+	{"^c#77bdff^ ^c#FFFFFF^", "sb-battery",	60,		0},
+	{"^c#77bdff^󰃞 ^c#FFFFFF^", "sb-backlight",	0,		2},
+	{"^c#77bdff^ ^c#FFFFFF^", "sb-volume",	0,		1},
+	{"", "sb-clock",	5,		0},
+	// {"^c#77bdff^ ^c#FFFFFF^", "sb-clock",	5,		0},
+	// {"",	"date",	60,	1},
+	// {"",	"sb-internet",	5,	4},
+	// /* {"",	"sb-iplocate", 0,	27}, */
+	// {"",	"sb-help-icon",	0,	15},
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = " | ";
+static unsigned int delimLen = 10;
